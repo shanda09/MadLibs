1c6ac9472b7521f5775210c82c30b6b438e091f1
shanda09 <32598920+shanda09@users.noreply.github.com>
2018-01-14 17:33:41 -0500

    eric's chnages

--- src/components/MadLib1.js
+++ src/components/MadLib1.js
@@ -2,27 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
+import MadLibAPI from "../api/MadLibAPI";
+
+
 const MadLib1 = () => (
   <div>
     <h1>
       MadLib1
-    </h1>
-    
-    <p>
-    I saw a (Person) with big (noun) on the train ride to (Place). To stop myself from staring, I (verb) my (noun). 
-    </p>
+   </h1>
 
     <p>
-    George.R.R Martin was so (verb) while writing Game of Thrones that he (adverb) (verb) everyone. 
-    </p>
+      I saw a {MadLibAPI.getAdj()} with big (noun) on the train ride to (Place). To stop myself from staring, I (verb) my (noun).
+   </p>
 
     <p>
-    (Noun) Bankrupts Make-A-Wish Foundation by wishing for so many (noun).
-    </p>
+      George.R.R Martin was so (verb) while writing Game of Thrones that he (adverb) (verb) everyone.
+   </p>
 
     <p>
-    When this girl was (verb) by her (Person), The internet came together and (adjective) told her to (verb).
-    </p>
+      (Noun) Bankrupts Make-A-Wish Foundation by wishing for so many (noun).
+   </p>
+
+    <p>
+      When this girl was (verb) by her (Person), The internet came together and (adjective) told her to (verb).
+   </p>
+
 
 
 
